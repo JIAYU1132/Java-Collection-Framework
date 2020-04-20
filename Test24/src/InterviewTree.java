9a7ab5ceb76205f6a1174bb4b69b3a033b174769
yjy <386902834@qq.com>
2020-04-20 09:31:56 +0800

    求二叉树的最佳深度

---
+++ Test24/src/InterviewTree.java
@@ -0,0 +1,32 @@
+import sun.reflect.generics.tree.Tree;
+
+import java.util.Map;
+
+class TreeNode {
+    public char val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(char val) {
+        this.val = val;
+    }
+}
+
+public class InterviewTree {
+    //给定一个二叉树，找出其最大深度。
+    //二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+    //说明: 叶子节点是指没有子节点的节点。
+    public int maxDepth(TreeNode root) {
+        // 最大深度即为 => Math.max(左子树深度, 右子树深度) + 1
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return 1 + Math.max(leftDepth, rightDepth);
+    }
+}
+
